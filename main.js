0f9c5dece91e20beae3d4ebbf735a9fcba2579a2
Jack WIlde <jackwilde630@gmail.com>
2016-09-14 15:47:23 -0400

    alternate between naughts and crosses each turn

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 $(function() {
 
+  var turn = 1
+
   $('.square').on('click', function() {
-    $(this).text("x");
-  });
+    square = $(this)
+    if (turn % 2 === 0) {
+      square.text("O")
+    } else {
+      square.text("X");
+    };
+    turn ++
+  })
+
 
 });
