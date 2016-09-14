071c00b666fbea20c32424014d0da5c22a587ee4
Jack WIlde <jackwilde630@gmail.com>
2016-09-14 17:19:17 -0400

    win conditions work

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 $(function() {
 
   var turn = 1
+  var xSquares = []
+  var oSquares = []
+
+  function didIWin(playerSquares) {
+    var arr = []
+    var countHash = {}
+    playerSquares.forEach(function(data) {
+      (data.split(" ")).forEach(function(thing) {
+        arr.push(thing);
+      });
+    });
+    arr.forEach(function(id) {
+      if (Object.keys(countHash).includes(id)) {
+        countHash[id] ++;
+      } else {
+        countHash[id] = 1;
+      }
+    })
+    Object.keys(countHash).forEach(function(key) {
+      if (countHash[key] === 3) {
+        alert("WINNER!");
+      }
+    })
+  }
 
   $('.square').on('click', function() {
     square = $(this)
-    if (turn % 2 === 0) {
-      square.text("O")
+    if (square.text() === '') {
+      if (turn % 2 === 0) {
+        square.text("O");
+        oSquares.push(square.data("wincons"));
+        didIWin(oSquares);
+      } else {
+        square.text("X");
+        xSquares.push(square.data("wincons"));
+        didIWin(xSquares);
+      };
+      turn ++
     } else {
-      square.text("X");
-    };
-    turn ++
+      alert("YOU SHALL NOT PASS!")
+    }
   })
 
 
